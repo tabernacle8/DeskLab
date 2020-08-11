07e8cc2319daf33a1acc5e449cfb41d92a31d853
Carter Kosturos <tabernacle800@gmail.com>
2020-08-11 09:34:06 -0700

    Setup starter code

--- deskLab.java
+++ deskLab.java
@@ -1,8 +1,15 @@
 public class deskLab {
     public static void main(String []args) {
 
-       //Type code here
+      //Runner code for testing
+      System.out.println(calculateDesks(100,100));
+       
+    }
 
+    public static int calculateDesks(int width, int height){
 
+      //Write code here
+
+      return(0);
     }
  }
