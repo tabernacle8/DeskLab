c29bbf0165e2d5f1db3aac946e76014490c4eec8
Carter Kosturos <tabernacle800@gmail.com>
2020-08-11 09:36:11 -0700

    Clarify

--- solution.java
+++ solution.java
@@ -1,13 +1,15 @@
-public class solution {
-    
+public class deskLab {
     public static void main(String []args) {
 
-        
-
-
+      //Runner code for testing
+      System.out.println(calculateDesks(100,100));
+       
     }
 
+    public static int calculateDesks(int width, int height){
 
+      //Write code here
 
-
-}
+      return(0);
+    }
+ }
